1bc64e85751201232690a94ea090ecf480ba714a
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-04-25 19:13:55 +0600

    base setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+//middleware
+app.use(cors());
+app.use(express.json());
